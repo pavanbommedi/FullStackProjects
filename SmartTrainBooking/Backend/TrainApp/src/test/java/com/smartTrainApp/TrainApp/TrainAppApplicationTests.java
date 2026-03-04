565be3b9934a6180dc9025547bc9d03ed90a7419
PavanKumar-2 <20h65a0209@cvsr.ac.in>
2026-03-05 00:51:52 +0530

    Started Smart Train Booking Application

---
+++ SmartTrainBooking/Backend/TrainApp/src/test/java/com/smartTrainApp/TrainApp/TrainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartTrainApp.TrainApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
