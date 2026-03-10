5f1795260501ae07816e849c6d68b976e13c8af0
PavanKumar-2 <20h65a0209@cvsr.ac.in>
2026-03-10 17:04:18 +0530

    implemented basic authentrication for Application

--- SmartTrainBooking/Backend/TrainApp/src/main/java/com/smartTrainApp/TrainApp/config/SecurityConfig.java
+++ SmartTrainBooking/Backend/TrainApp/src/main/java/com/smartTrainApp/TrainApp/config/SecurityConfig.java
@@ -1,28 +1,56 @@
 package com.smartTrainApp.TrainApp.config;
 
+// package com.smartTrainApp.TrainApp.config;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.Customizer;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
+
+// @Configuration
+// public class SecurityConfig {
+
+//     @Bean
+//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+//         http
+//                 .csrf(csrf -> csrf.disable())   // disable csrf for h2
+//                 .authorizeHttpRequests(auth -> auth
+//                         .requestMatchers("/h2-console/**").permitAll()
+//                         .anyRequest().authenticated()
+//                 )
+//                 .headers(headers -> headers
+//                         .frameOptions(frame -> frame.disable())  // allow iframe
+//                 )
+//                 .httpBasic(Customizer.withDefaults());
+
+        
+
+//         return http.build();
+//     }
+// }
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer.AuthorizedUrl;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http
-                .csrf(csrf -> csrf.disable())   // disable csrf for h2
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .headers(headers -> headers
-                        .frameOptions(frame -> frame.disable())  // allow iframe
-                )
-                .httpBasic(Customizer.withDefaults());
-
-        return http.build();
-    }
+@EnableWebSecurity
+public class SecurityConfig{
+        @Bean
+        SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+            http.authorizeHttpRequests((requests) -> {
+                ((AuthorizedUrl)requests.anyRequest()).authenticated();
+            });
+            http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            // http.formLogin(Customizer.withDefaults());
+            http.httpBasic(Customizer.withDefaults());
+            return (SecurityFilterChain)http.build();
+        }
 }
